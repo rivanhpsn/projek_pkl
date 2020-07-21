5fec2f7d967512e5b1b2e2f68ec395cf3fa0fec2
Rivan <43029672+Posan20@users.noreply.github.com>
2020-07-21 21:14:17 +0700

    Templating Admin LTE dan Tabel Pelanggan

---
+++ database/migrations/2020_07_20_122203_create_pelanggan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelangganTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('contact_person');
+            $table->string('no_polisi');
+            $table->string('model_type');
+            $table->string('no_telp');
+            $table->string('retention_out');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggan');
+    }
+}
