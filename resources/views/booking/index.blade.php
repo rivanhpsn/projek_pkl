a4ca1f74a80704af5a6341df7099466b62194835
Rivan <43029672+Posan20@users.noreply.github.com>
2020-08-13 15:45:12 +0700

    Tabel Data Responsive
    Menambahkan plugin responsive pada setiap tabel

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -52,9 +52,14 @@
 <!-- DataTables -->
 <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 <script>
     $(document).ready(function(){
-        $('#datatable').DataTable();
+        $('#datatable').DataTable({
+            "responsive": true,
+            "autoWidth": false,
+        });
     });
 </script>
 @endpush
@@ -62,5 +67,6 @@
 @push('cssDT')
 <!-- DataTables -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 
 @endpush
--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -51,9 +51,14 @@
 <!-- DataTables -->
 <script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 <script>
     $(document).ready(function(){
-        $('#datatable').DataTable();
+        $('#datatable').DataTable({
+            "responsive": true,
+            "autoWidth": false,
+        });
     });
 </script>
 @endpush
@@ -61,5 +66,6 @@
 @push('cssDT')
 <!-- DataTables -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 
 @endpush
